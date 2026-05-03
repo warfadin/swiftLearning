a48b34f0c77b60fb418502077fdbe18a5f90ae78
Mehmet Ataman <ataman96@outlook.com>
2026-05-03 14:27:44 +0300

    class ve struct farkları

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,57 +1,32 @@
 import Cocoa
 
-// static, mutating, throwing örneği
-// gear kontrolü ve error handling
-// Paul Hudson checkpoint 6'yı çözmeye çalıştım ama bokunu çıkardım bence, yine de kötü değil.
+// class ve struct arasındaki farklar:
+// class üstüne class girebilirsin (inheritance)
+// class'larda swift kendi default memberwise init yapmıyor
+// class içerisinde bir instance'ın kopyasını oluşturup instance'ı değiştirirsen orijinal class'taki datayı da değiştirir.
+// deinitializer ile final kopya destroy edilebilir .
+// constant class instance'ların varieble property'leri değiştirilebilir.
 
-
-struct Car {
-    enum CarError: Error {
-        case gearNotAvailable
-    }
-    let model: String
-    let year: Int
-    let numberOfSeats: Int
-    static let maxGear = 6
-    private(set) var currentGear: Int {
-        willSet {
-            print("Gear is changing...")
-        }
+class Game {
+    var score = 0 {
         didSet {
-            print("Current gear is now: \(currentGear)")
-        }
-    }
-    init(model: String, year: Int, numberOfSeats: Int, currentGear: Int) throws {
-        if currentGear > Car.maxGear || currentGear <= 0 {
-            throw CarError.gearNotAvailable
-        }
-        self.currentGear = currentGear
-        self.model = model
-        self.year = year
-        self.numberOfSeats = numberOfSeats
-    }
-    
-    mutating func gearUp(amount gear: Int) throws {
-        if gear <= 0 || gear + currentGear > Car.maxGear {
-            throw CarError.gearNotAvailable
-        } else {
-            currentGear += gear
-        }
-    } // mutating func burada bitiyor.
-    
-    mutating func gearDown(amount gear: Int) throws {
-        if gear <= 0 || currentGear - gear <= 0 {
-            throw CarError.gearNotAvailable
-        } else {
-            currentGear -= gear
+            print("\(score)")
         }
     }
 }
 
-do {
-    var myCar = try Car(model: "Tesla", year: 2020, numberOfSeats: 5, currentGear: 1)
-    try myCar.gearUp(amount: 5)
-    try myCar.gearDown(amount: 1)
-} catch Car.CarError.gearNotAvailable {
-    print ("Gear is invalid")
+var newGame = Game()
+newGame.score = 10
+
+// ile alttaki şey aynı aslında
+
+struct Game1 {
+    var score = 0 {
+        didSet {
+            print("\(score)")
+        }
+    }
 }
+
+var newGame2 = Game1()
+newGame2.score = 10
