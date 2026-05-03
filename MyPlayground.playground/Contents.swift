a47dbb41ff66783a3a9dbe91a9e58907dde87b80
Mehmet Ataman <ataman96@outlook.com>
2026-05-03 16:48:25 +0300

    Class ve struct işlerinde kopyalama mevzusu, kısaca class reference verirken struct gerçek kopya yapıyor.

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,44 +1,56 @@
 import Cocoa
 
-
-// class'lar için initializers konusundayım.
-
-class Vehicle {
-    let isElectric: Bool
-
-    init(isElectric: Bool) {
-        self.isElectric = isElectric
-    }
+class User {
+    var username = "Anonymous"
 }
 
-// class Car: Vehicle {
-//    let isConvertible: Bool
+var user1 = User()
+user1.username = "Foo"
 
-//    init(isConvertible: Bool) {
-//        self.isConvertible = isConvertible
-//    }
-// }
+var user2 = user1
+user2.username = "Bar"
 
-// yukarıdaki halini Swift kabul etmiyor, çünkü Vehicle'dan inherit'leyen bir Car yaptık ama isElectric sorusuna cevabımız yok..
-// O yüzden:
+print(user1.username)
+print(user2.username)
 
-class Car1: Vehicle {
-    let isConvertible: Bool
+// burada user1 User class'ından bir nesne oluşturdu ve username'i "Foo" yaptı.
+// user2 = user1 dediğimizde yeni bir kopya oluşmadı, aynı User nesnesine referans verildi.
+// bu yüzden user2 üzerinden yapılan değişiklik (username = "Bar"), aslında aynı nesne üzerinde gerçekleşti.
+// dolayısıyla hem user1 hem user2 için username "Bar" olarak göründü.
+
+// _________________
+
+// eğer struct kullansaydık bunlar ayrı olurdu, çünkü struct'ta referans alınmıyor, kopya oluşturuluyor.
+
+struct UserX {
+    var username = "Anonymous"
+}
+
+var user1x = UserX()
+user1x.username = "Foo"
+
+var user2x = user1x
+user2x.username = "Bar"
+
+print(user1x.username)
+print(user2x.username)
+
+//
+
+class UserCustom {
+    var username = "Anonymous"
     
-    init(isElectric: Bool, isConvertible: Bool) {
-        self.isConvertible = isConvertible // car1 içindeki kendi propertyimiz
-        super.init(isElectric: isElectric) // Vehicle içindeki property
+    func copy() -> UserCustom {
+        let user = UserCustom()
+        user.username = username
+        return user
     }
 }
 
-let tesla = Car1(isElectric: true, isConvertible: false)
+var user3 = UserCustom()
+user3.username = "Foo"
 
-// bununla birlikte, eğer child class'ın içinde init etmek istediğimiz bir şey yoksa, class'ın içindeki property'e örneğin default bir value vererek bütün custom init'i silip, Vehicle'daki init'i otomatik inherit etmesini sağlayabiliriz. bu durumda:
-
-class Car2: Car1 {
-    let isManual = false // property'e default olarak bi değer verdiğim için gerikalan (yani aslında "üstlerde kalan" propertylerin init'lerini Swift otomatik olarak init'liyor.
-    }
-
-let toyota = Car2(isElectric: false, isConvertible: false)
-
-print(toyota.isManual)
+var user4 = user3.copy()
+user4.username = "Mehmet"
+print(user4.username)
+print(user3.username)
