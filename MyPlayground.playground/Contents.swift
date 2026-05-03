700ea317a300ee842224f260a39e6c92a12d2e7b
Mehmet Ataman <ataman96@outlook.com>
2026-05-03 15:26:13 +0300

    classların içindeki initializersların nasıl yapıldığının öğrenilmesi

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,46 +1,44 @@
 import Cocoa
 
 
-// inheritance konusundayım.
+// class'lar için initializers konusundayım.
 
+class Vehicle {
+    let isElectric: Bool
 
-class Employee {
-    let hours: Int
-    init(hours: Int) {
-        self.hours = hours
-    }
-    func printSummary() {
-        print ("I work \(hours) a day")
+    init(isElectric: Bool) {
+        self.isElectric = isElectric
     }
 }
 
+// class Car: Vehicle {
+//    let isConvertible: Bool
 
-// class Developer'ın önüne final eklersem, bu artık son child class olur ve artık Developer class'ından daha child bir class oluşturulamaz.
+//    init(isConvertible: Bool) {
+//        self.isConvertible = isConvertible
+//    }
+// }
 
-class Developer: Employee { // üstteki Employee class'ından bütün özellikleri inheritlemiş oldu, init dahil.
-    func work() { // buradaki func ile aşağıdaki func'ın ismi aynı olmasına rağmen Developer de Manager de yine Employee class'ından inheritlendiği için çağrılırken swift bunun hangi class'tan geldiğini anlayacak.
-        print("I'm writing code for \(hours) hours")
-    }
-    override func printSummary() {
-        print("I'm a developer who will sometimes work \(hours) hours a day, but other times will spend hours arguing.")
+// yukarıdaki halini Swift kabul etmiyor, çünkü Vehicle'dan inherit'leyen bir Car yaptık ama isElectric sorusuna cevabımız yok..
+// O yüzden:
+
+class Car1: Vehicle {
+    let isConvertible: Bool
+    
+    init(isElectric: Bool, isConvertible: Bool) {
+        self.isConvertible = isConvertible // car1 içindeki kendi propertyimiz
+        super.init(isElectric: isElectric) // Vehicle içindeki property
     }
 }
 
-class Manager: Employee {
-    func work () {
-        print("I'm going to meetings for \(hours)")
+let tesla = Car1(isElectric: true, isConvertible: false)
+
+// bununla birlikte, eğer child class'ın içinde init etmek istediğimiz bir şey yoksa, class'ın içindeki property'e örneğin default bir value vererek bütün custom init'i silip, Vehicle'daki init'i otomatik inherit etmesini sağlayabiliriz. bu durumda:
+
+class Car2: Car1 {
+    let isManual = false // property'e default olarak bi değer verdiğim için gerikalan (yani aslında "üstlerde kalan" propertylerin init'lerini Swift otomatik olarak init'liyor.
     }
-}
 
-let robert = Developer(hours: 10) // robert'i developer yaptı ve Employee class'ındaki hours propertysini kullanarak Developer içindeki work methodunu çalıştırdı.
-let joseph = Manager(hours: 10) // aynısını Employee'den inheritleyip manager'daki work methodunu çalıştırdık.
-robert.work() // robert instance'ının içinde work methodunu kullandı, robert bir developer olduğu için onun içindeki work methodunu kullandığı için "writing code ...." şeklinde çıktı verdi.
-joseph.work()
+let toyota = Car2(isElectric: false, isConvertible: false)
 
-let mehmet = Manager(hours: 12)
-mehmet.printSummary() // burada mehmet'i manager yaptık ve 12. satırdaki Employee classındaki method Developer için de geçerli olduğu için direk kullanabildik.
-
-let altay = Developer(hours: 2)
-altay.printSummary() // burada altayı Developer yaptık ve Developer class'ının içindeki override func'ında ana Class (Employee'deki) printSummary'ı modify ettiğimiz için artık developer içindeki override edilmiş methodun sonucunu gösteriyor.
-
-
+print(toyota.isManual)
