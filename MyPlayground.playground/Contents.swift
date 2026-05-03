c2ce25d87af7cbb29b1e51f62e5186b2ed62f2f7
Mehmet Ataman <ataman96@outlook.com>
2026-05-03 15:08:25 +0300

    Class Inheritance dersi

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,32 +1,46 @@
 import Cocoa
 
-// class ve struct arasındaki farklar:
-// class üstüne class girebilirsin (inheritance)
-// class'larda swift kendi default memberwise init yapmıyor
-// class içerisinde bir instance'ın kopyasını oluşturup instance'ı değiştirirsen orijinal class'taki datayı da değiştirir.
-// deinitializer ile final kopya destroy edilebilir .
-// constant class instance'ların varieble property'leri değiştirilebilir.
 
-class Game {
-    var score = 0 {
-        didSet {
-            print("\(score)")
-        }
+// inheritance konusundayım.
+
+
+class Employee {
+    let hours: Int
+    init(hours: Int) {
+        self.hours = hours
+    }
+    func printSummary() {
+        print ("I work \(hours) a day")
     }
 }
 
-var newGame = Game()
-newGame.score = 10
 
-// ile alttaki şey aynı aslında
+// class Developer'ın önüne final eklersem, bu artık son child class olur ve artık Developer class'ından daha child bir class oluşturulamaz.
 
-struct Game1 {
-    var score = 0 {
-        didSet {
-            print("\(score)")
-        }
+class Developer: Employee { // üstteki Employee class'ından bütün özellikleri inheritlemiş oldu, init dahil.
+    func work() { // buradaki func ile aşağıdaki func'ın ismi aynı olmasına rağmen Developer de Manager de yine Employee class'ından inheritlendiği için çağrılırken swift bunun hangi class'tan geldiğini anlayacak.
+        print("I'm writing code for \(hours) hours")
+    }
+    override func printSummary() {
+        print("I'm a developer who will sometimes work \(hours) hours a day, but other times will spend hours arguing.")
     }
 }
 
-var newGame2 = Game1()
-newGame2.score = 10
+class Manager: Employee {
+    func work () {
+        print("I'm going to meetings for \(hours)")
+    }
+}
+
+let robert = Developer(hours: 10) // robert'i developer yaptı ve Employee class'ındaki hours propertysini kullanarak Developer içindeki work methodunu çalıştırdı.
+let joseph = Manager(hours: 10) // aynısını Employee'den inheritleyip manager'daki work methodunu çalıştırdık.
+robert.work() // robert instance'ının içinde work methodunu kullandı, robert bir developer olduğu için onun içindeki work methodunu kullandığı için "writing code ...." şeklinde çıktı verdi.
+joseph.work()
+
+let mehmet = Manager(hours: 12)
+mehmet.printSummary() // burada mehmet'i manager yaptık ve 12. satırdaki Employee classındaki method Developer için de geçerli olduğu için direk kullanabildik.
+
+let altay = Developer(hours: 2)
+altay.printSummary() // burada altayı Developer yaptık ve Developer class'ının içindeki override func'ında ana Class (Employee'deki) printSummary'ı modify ettiğimiz için artık developer içindeki override edilmiş methodun sonucunu gösteriyor.
+
+
