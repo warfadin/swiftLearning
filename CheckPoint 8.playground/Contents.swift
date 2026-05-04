c50f08830655b97eebd5af80c33ba1fab0c72d93
Mehmet Ataman <ataman96@outlook.com>
2026-05-05 00:47:44 +0300

    optionals

--- CheckPoint 8.playground/Contents.swift
+++ CheckPoint 8.playground/Contents.swift
@@ -1,3 +1,36 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+protocol Building {
+    var roomCount: Int { get }
+    var cost: Int { get }
+    var type: String { get }
+    var estateAgent: String { get }
+    func salesSummary() -> String
+}
+
+extension Building {
+    func salesSummary() -> String {
+        return "\(estateAgent) is selling a \(type) with \(roomCount) rooms for \(cost) dollars"
+    }
+}
+
+struct House: Building {
+    var roomCount: Int
+    var cost: Int
+    var estateAgent: String
+    var type = "house"
+}
+
+struct Office: Building {
+    var roomCount: Int
+    var cost: Int
+    var estateAgent: String
+    var type = "office"
+}
+
+let house1 = House(roomCount: 3, cost: 100_000, estateAgent: "Bob")
+let office1 = Office(roomCount: 10, cost: 1_000_000, estateAgent: "Bob")
+
+print(house1.salesSummary())
+print(office1.salesSummary())
+
---
+++ Test1/Test1/Test1App.swift
@@ -0,0 +1,17 @@
+//
+//  Test1App.swift
+//  Test1
+//
+//  Created by Mehmet Ataman on 4.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct Test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
