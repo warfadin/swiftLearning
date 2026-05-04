3cd50e8987ed4d19c1778797c292dfeaecd12a6c
Mehmet Ataman <ataman96@outlook.com>
2026-05-05 01:19:15 +0300

    guard let

--- Day 14.playground/Contents.swift
+++ Day 14.playground/Contents.swift
@@ -1,1 +1,28 @@
+// GUARD LET
 
+func printSquare(of number: Int?) {
+    guard let number = number else {
+        print("Missing input")
+        return // guard let her zaman fonksiyondan çıkmaya bi yol vermeli
+    }
+
+    print("\(number) x \(number) is \(number * number)")
+}
+
+/////
+func getMeaningOfLife() -> Int? {
+    42
+}
+
+func printMeaningOfLife() {
+    if let name = getMeaningOfLife() {
+        print(name)
+    }
+}
+
+func printM() {
+    guard let name = getMeaningOfLife() else {
+        return
+    }
+    print(name)
+}
