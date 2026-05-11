285799f1d61ebbe2f99f98ea481971c43c0746e2
warfadin <ataman96@outlook.com>
2026-05-11 13:35:24 +0300

    extraction

--- Vazoaktif Ajanlar/MedKitPlus/Features/VasoactiveCalculator/VasoactiveCalculatorFormatting.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-enum VasoactiveCalculatorFormatting {
-    static func cleanNumber(_ value: Double) -> String {
-        value.truncatingRemainder(dividingBy: 1) == 0 ? String(Int(value)) : String(value)
-    }
-}
--- Vazoaktif Ajanlar/MedKitPlus/Shared/KeyboardDismissal.swift
+++
@@ -1,9 +0,0 @@
-#if os(iOS)
-import UIKit
-#endif
-
-func dismissKeyboard() {
-    #if os(iOS)
-    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-    #endif
-}
